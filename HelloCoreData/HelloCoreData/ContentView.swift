41d829b60f1dcbd7c1cc8e5e2b7f225c8280ed1d
Kirill SIMAGIN <ksimagin@gmail.com>
2023-03-20 21:35:09 +0100

    apply some funny stuffs

--- HelloCoreData/HelloCoreData/ContentView.swift
+++ HelloCoreData/HelloCoreData/ContentView.swift
@@ -9,14 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     let storageProvider: StorageProvider
+    @State private var text: String = ""
     var body: some View {
+        dataList
+    }
+    private func onTap(text: Binding<String>?){
+        guard let value = text?.wrappedValue,
+              !value.isEmpty else {
+                  return
+              }
+        storageProvider.saveMovie(named: value)
+        $text.wrappedValue = ""
+    }
+    
+    var header: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                TextField("Placeholder", text: $text)
+                Button("add") {
+                   onTap(text: $text)
+                }
+            }
         }
-        .padding()
+    }
+    var dataList: some View {
+        List{
+            Section {
+                ForEach(storageProvider.getAllMovies(),id: \.self){ movie in
+                    listElemet(movie: movie)
+                }.onDelete(perform: delete)
+            } header: {
+                header
+            }
+        }
+        .listStyle(.automatic)
+    }
+    @ViewBuilder func listElemet(movie: Movie) -> some View{
+        Text(movie.name ?? "empty movie")
+    }
+    private func delete(at offsets: IndexSet) {
+        // delete the objects here
+        let movies = storageProvider.getAllMovies()
+        let toDelete = offsets.compactMap{movies[$0]}
+        toDelete.forEach{
+            storageProvider.deleteMovie($0)
+        }
     }
 }
 
